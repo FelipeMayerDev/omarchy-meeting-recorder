f99d8154aaec579ab585ee4ef92c53d1e3780161
Felipe Mayer <mayer.felipe@outlook.com>
2026-09-25 15:03:31 -0300

    Revert "Use NeMo for live remote diarization"
    This reverts commit f37b89b8c4b9539adace1e170d3a3b66f022b3fc.

--- src/ui.rs
+++ src/ui.rs
@@ -1938,7 +1938,8 @@ impl Recorder {
         *self.live_abort.borrow_mut() = Some(abort.clone());
         let (updates, received) = async_channel::unbounded();
         let chunks = self.system.subscribe();
-        std::thread::spawn(move || crate::live::run(chunks, provider, updates, abort));
+        let speakers = self.selected_remote_speaker_count();
+        std::thread::spawn(move || crate::live::run(chunks, provider, speakers, updates, abort));
         let weak = Rc::downgrade(self);
         glib::spawn_future_local(async move {
             while let Ok(update) = received.recv().await {
